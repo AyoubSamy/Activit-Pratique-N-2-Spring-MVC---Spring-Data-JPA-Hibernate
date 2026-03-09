82f93cda0ba5cef180d406178ab6a215c67f6929
7ayoub15 <SAMY82696@GMAIL.COM>
2026-03-09 13:12:07 +0000

    adding ProductController and thymeleaf page .html

---
+++ src/main/java/net/ayoub/ap2springmvc/web/ProductController.java
@@ -0,0 +1,23 @@
+package net.ayoub.ap2springmvc.web;
+
+import net.ayoub.ap2springmvc.entities.Product;
+import net.ayoub.ap2springmvc.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    public ProductRepository productRepository;
+
+    @GetMapping("/index")
+    public String index(Model model){
+        List<Product> products  = productRepository.findAll();
+        model.addAttribute("productList",products);
+        return "products";
+    }
+}
